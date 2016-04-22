8a9d95e65cf750cd4d16b4cc7b644be3795b6f69
gbabu100 <ganeshbabu.thavasimuthu@gmail.com>
2016-04-22 18:45:22 +0530

    send receipt hard coded

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ function sendReceiptTemplate(sender){
       "payload": {
         "template_type": "receipt",
         "recipient_name": "Stephane Crozatier",
-        "order_number": "12345678902",
+        "order_number": "1234",
         "currency": "USD",
         "payment_method": "Visa 2345",
         "order_url": "http://petersapparel.parseapp.com/order?order_id=123456",
