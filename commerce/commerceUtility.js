b632c448224f6c94daee447c77d3ba90082bb0dc
gbabu100 <ganeshbabu.thavasimuthu@gmail.com>
2016-04-23 00:08:39 +0530

    sdfsf

--- commerce/commerceUtility.js
+++ commerce/commerceUtility.js
@@ -72,7 +72,7 @@ function getProducts(categoryId, sender){
   }, function(error, response, body) {
     if (!error) {
       console.log('Success getProducts');
-		var catentryArray = body.CatalogEntryView;
+		var catentryArray = body.recordSetTotal;
     console.log('catentryArray',catentryArray);
 		for (var i = 0; i < 5; i++) {
       console.log('catentryArray[i].name'+catentryArray[i].name);
