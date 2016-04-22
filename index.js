3f264284ddfdd1ab8337ad325450f0a0877c4540
Karunanidhi Devanidhi <karunanidhid@Karunanidhimacbook.local>
2016-04-22 10:30:48 +0530

    local changes

--- index.js
+++ index.js
@@ -33,13 +33,21 @@ server.post('/webhook/', function (req, res) {
       console.log('text '+text);
       if (text == 'Generic') {
       	console.log('Inside Generic '+text);
-    	sendGenericMessage(sender);
-    	continue;
+    	  sendGenericMessage(sender);
+    	  continue;
   	  }else if(text == 'Asdf'){
   	  	console.log('Inside Hi '+text);
         sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200)); 	
         continue;  	
-  	  }
+  	  }else if (text === 'Hello1234') {
+        sendTextMessage(sender, text.substring(0, 200));
+        delete sessions[sessionId];
+      }else {
+        const sessionId = findOrCreateSession(sender);
+        wit.runActions(sessionId, text, sessions[sessionId].context, (error, context) => {
+          if (error) console.log(error);
+        });
+      }
     }
     if (event.postback) {
     	text = JSON.stringify(event.postback);
@@ -127,6 +135,152 @@ function sendGenericMessage(sender) {
     } else if (response.body.error) {
       console.log('Error: ', response.body.error);
     }
+  });
+}
+
+const WIT_TOKEN = "KMG23FM4RVSUDOUJ2FB3V4GYZJADLIRJ";
+
+const Wit = require('wit').Wit;
+const uuid = require('node-uuid');
+
+const HERE = {
+  id:"xqatBOxmf61Jv8AzLoS9",
+  code: "EopBF0eLNTAcVNzo297DDA",
+}
+
+const currentLocation = { //Fort Mason
+  Latitude: 37.80524,
+  Longitude: -122.42818
+}
+
+const sessions = {};
+
+const actions = {
+  say: (sessionId, msg, cb) => {
+    const recipient = sessions[sessionId].fbid;
+    if (recipient) {
+      sendTextMessage(recipient, msg); 
+        cb();
+    } else {
+      cb();
+    }
+  },
+  merge: (context, entities, cb) => {
+    const elocation = firstEntityValue(entities, 'location');
+    const emode = firstEntityValue(entities, 'mode');
+    if (elocation) context.location = elocation;
+    if (emode) context.mode = emode;
+    cb(context);
+  },
+  fetchTraffic: (context, cb) => {
+    // Here should go the api call, e.g.:
+    getTraffic(context.location,context.mode,(time,error) => {
+      context.travel_time = time;
+      cb(context);
+    });
+  },
+  error: (sessionId, msg) => {
+    const recipient = sessions[sessionId].fbid;
+    if (recipient) {
+      sendTextMessage(recipient, 'Oops, I don\'t know what to do.');
+    }
+  },
+};
+
+const wit = new Wit(WIT_TOKEN, actions);
+
+const findOrCreateSession = (fbid) => {
+  var sessionId;
+  for (const key in sessions) {
+    if (sessions.hasOwnProperty(key)) {
+      if (sessions[key].fbid == fbid) {
+        sessionId = key;
+        break;
+      }
+    }
+  }
+  if (!sessionId) {
+    sessionId = uuid.v1();
+    sessions[sessionId] = { fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const getGeoLoc = (location, callback) => {
+  if(location === undefined)
+    callback(undefined, "no location captured");
+  else {
+    request({
+        url: 'https://geocoder.cit.api.here.com/6.2/geocode.json',
+        qs: {app_id: HERE.id,
+           app_code: HERE.code,
+           gen:9,
+           city: 'San Francisco',
+           searchText: location.replace(/ /g, '+')},
+           method: 'GET',
+           json: true,
+    }, function(error, response, body){
+        if(error) {
+            callback(undefined, error);
+        } else {
+            callback(body.Response.View[0].Result[0].Location.DisplayPosition);
+
+        }
+    });
+  }
+}
+
+const getTravelTime = (from, to, mode, callback) => {
+  var traffic_mode = mode
+  if (mode === undefined) {
+    traffic_mode = "car";
+  }
+  request({
+      url: 'https://route.cit.api.here.com/routing/7.2/calculateroute.json',
+      qs: {app_id: HERE.id,
+         app_code: HERE.code,
+         mode: "fastest;"+traffic_mode+";traffic:enabled",
+         waypoint0: "geo!"+ from.Latitude + ','+ from.Longitude,
+         waypoint1: "geo!"+ to.Latitude + ','+ to.Longitude},
+         method: 'GET',
+         json: true,
+  }, function(error, response, body){
+      if(error) {
+          callback(undefined, error);
+      } else {
+        if(body.response)
+            callback(body.response.route[0].summary.travelTime);
+          else
+            callback(undefined,JSON.stringify(body));   
+      }
+  });
+}
+
+const getTraffic = (destination, mode, callback) => {
+  getGeoLoc(destination,(displayPosition,error)=> {
+    if(!error)
+      getTravelTime(currentLocation,displayPosition, mode,(travelTime,error) => {
+        if(!error) {
+          //console.log('It will take ' + Math.ceil(trafficTime/60) + 'min to go to ' + destination)
+          callback(Math.ceil(travelTime/60));
+        }
+        else
+          callback(undefined, error);
+      });
+    else
+      callback(undefined, error);
   });
 }
 
